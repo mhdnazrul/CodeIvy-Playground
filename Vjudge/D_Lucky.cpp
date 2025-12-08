910598440f1c1be625a02c49e49b9838bd9342cc
Nazrul Islam <mhdnazrul511@gmail.com>
2025-12-08 13:20:54 +0600

    Add new solutions

---
+++ Vjudge/D_Lucky.cpp
@@ -0,0 +1,31 @@
+// problem link:https://vjudge.net/contest/772614#problem/D
+// Author: nazrulislam_7
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define nl '\n'
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+template<class T> void readV(vector<T>& v) { for(auto &x : v) cin >> x; }
+template<class T> void printV(const vector<T>& v) { for(auto x : v) cout << x << ' '; cout << '\n'; }
+
+void solve() {
+    string s;   cin>>s;
+    int first = 0, sd = 0;
+    for(int i=0; i<6; i++){
+        if(i<3){first += s[i] -'0';}
+        else {sd += s[i] -'0';}
+    }
+    cout<<(first==sd? "YES\n":"NO\n");
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int test_cases = 1;
+    if(!(cin>>test_cases)) return 0;
+    for(int tc = 1; tc <= test_cases; tc++){
+    //  cout << "Case #" << tc << ": ";
+        solve();
+    }
+    return 0;
+}
---
+++ Vjudge/E_Equal_Candies.cpp
@@ -0,0 +1,39 @@
+// problem link:https://vjudge.net/contest/772614#problem/E
+// Author: nazrulislam_7
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define nl '\n'
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+template<class T> void readV(vector<T>& v) { for(auto &x : v) cin >> x; }
+template<class T> void printV(const vector<T>& v) { for(auto x : v) cout << x << ' '; cout << '\n'; }
+
+void solve() {
+    ll n;      cin>>n;
+    vector<ll> v(n);
+    ll mn = LONG_MAX;
+    ll total_sum = 0;
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+        mn = min(mn,v[i]);
+    }
+    for(int i=0; i<n; i++){
+        ll x = v[i] - mn;
+       // cout<< "V["<< i<<"] == x: " <<x<<nl;
+        total_sum += x;
+    }
+    cout<< total_sum<<nl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int test_cases = 1;
+    if(!(cin>>test_cases)) return 0;
+    for(int tc = 1; tc <= test_cases; tc++){
+    //  cout << "Case #" << tc << ": ";
+        solve();
+    }
+    return 0;
+}
+
